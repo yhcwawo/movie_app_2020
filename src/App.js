c0f426038f5e7daf055a60c391e6d661e921e462
yhc <ghdcks1018@gmail.com>
2021-01-07 02:10:48 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    } , 3000);
+
+  };
+
+  
+    render(){
+
+      const { isLoading} = this.state;
+
+      return (
+
+
+        <div>{isLoading ? "Loading..." : "We are ready"} </div>
+
+      );
+    }
+
+
 }
+
+      
+
+
 
 export default App;
